0305fd1eed04b0328a1048991daa53b6deb191b1
DarkGodFenrir <0xaker@gmail.com>
2021-02-21 09:40:26 +0300

    add sql

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 import telebot
 import param
+from sqlline import *
 from keys import *
 
 from telethon.sync import TelegramClient
@@ -14,12 +15,24 @@ from telethon.tl.types import ChannelParticipantsSearch
 
 bot = telebot.TeleBot(param.TOKEN)
 
+api_id   = param.API_ID
+api_hash = param.API_HASH
+username = param.USERNAME
+
+client = TelegramClient(username, api_id, api_hash)
+client.start()
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
+
+    user = [message.from_user.id, message.from_user.username]
+    prov = Sqldb.r_users(user)
     key = Keys.main_keys()
-    bot.send_message(message.chat.id, 'Доброго времени суток, этот бот создан для того, чтобы обьединить информацию из нескольких групп.', reply_markup = key)
 
-
+    if prov is True:
+        bot.send_message(message.chat.id, 'С возвращением!', reply_markup = key)
+    else:
+        bot.send_message(message.chat.id, 'Доброго времени суток, этот бот создан для того, чтобы обьединить информацию из нескольких групп.', reply_markup = key)
 
 
 
---
+++ sqlline.py
@@ -0,0 +1,33 @@
+import sqlite3
+import re
+
+conn = sqlite3.connect('news.db')
+
+class Sqldb:
+
+    def r_users(user_f):
+
+        id = user_f[0]
+
+        conn = sqlite3.connect('news.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT uid FROM main ORDER BY uid")
+        prow = cursor.fetchall()
+
+        for och in prow:
+            prow = re.sub(r"[(,)]","",str(prow))
+
+        if str(user_f[0]) not in prow:
+
+            with conn:
+                cursor = conn.cursor()
+                #cursor.execute('INSERT INTO user(uid, name) VALUES (?, ?)', (user_f))
+                cursor.execute('INSERT INTO main(uid,uname) VALUES (?,?)', (user_f))
+
+                cursor.close()
+                return False
+        else:
+            with conn:
+
+                cursor.close()
+            return True
